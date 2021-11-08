685825c0e158ce8eb8d6c2915a5519d131f6cf8f
DanielC12321 <73292458+DanielC12321@users.noreply.github.com>
2021-11-08 08:00:47 -0600

    Update apitest.py

--- apitest.py
+++ apitest.py
@@ -1,5 +1,9 @@
 import requests
 import json
-response = requests.get("https://deckofcardsapi.com/api/deck/new/draw/?count=2")
+response = requests.get("https://deckofcardsapi.com/api/deck/new/draw/?count=10")
+x = response.json()
 print(response.status_code)
 print(json.dumps(response.json(),indent=4) )
+for card in x['cards']:
+    print(card['code'])
+    print(card['image'])
