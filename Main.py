1bf4320f9eaf012b278497e6eb05b7cb504eb764
Khangnx03 <73293627+Khangnx03@users.noreply.github.com>
2021-11-08 07:29:51 -0600

    alsdjf

--- Main.py
+++ Main.py
@@ -1,1 +1,22 @@
+import pygame
+pygame.init()
 
+window = pygame.display.set_mode((500, 500))
+pygame.display.set_caption("Trash")
+
+x = 50
+y = 50
+width = 40
+height = 60
+vel = 5
+
+run = True
+while(run):
+    pygame.time.delay(100)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+    pygame.draw.rect()
+pygame.quit()
