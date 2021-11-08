269cab1e4bdf8462aacd1445a10017a14b956c44
Khangnx03 <73293627+Khangnx03@users.noreply.github.com>
2021-11-08 07:40:41 -0600

    dfasd

--- Main.py
+++ Main.py
@@ -5,13 +5,21 @@ WIDTH, HEIGHT = 900, 500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Trash")
 
+WHITE = (255, 255, 255)
+
+def draw_window():
+    WIN.fill(WHITE)
+    pygame.display.update()
+
 def main():
     run = True
     while run:
         for event in pygame.event.get():
             if event.type  == pygame.QUIT:
                 run = False
-    
+
+       draw_window()
+
     pygame.quit()
 
 if __name__ == "__main__":
