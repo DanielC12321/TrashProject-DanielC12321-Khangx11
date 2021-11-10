330ca68b8dc2595a6098a3e43ba5f4237e2e27c9
DanielC12321 <73292458+DanielC12321@users.noreply.github.com>
2021-11-10 08:03:01 -0600

    Update deckmethods.py

--- deckmethods.py
+++ deckmethods.py
@@ -1,33 +1,22 @@
 import requests
 import json
-<<<<<<< HEAD
-response = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-deckid = (response.json())['deck_id']
-def getCard(numcards):
-    print(requests.get('https://deckofcardsapi.com/api/deck/{id}/draw/?count={number}'.format(id = str(deckid), number = int(numcards)).dumps()))
-getCard(5)
-=======
 class deckmethod:
     response = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-    
+    discard = []
     deckid = (response.json())['deck_id']
-    def getCard(self, numcards):
+    #takes in tuple with cardname and link to image
+    def disardcard(self, card):
+        self.discard.append(card)
+    #draws number of cards
+    def getcard(self, numcards):
         x =  (requests.get('https://deckofcardsapi.com/api/deck/' +str(self.deckid)+'/draw/?count='+str(numcards))).json()
         cardlist = []
         for card in x['cards']:
             cardlist.append((card['code'],card['image']))
 
         return cardlist
-
-    def getNew(self):
+    #creates new deck
+    def getnew(self):
         self.deckid = (requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").json())['deck_id']
-        pass
+        
 
-
-
-a = deckmethod()
-print(a.deckid)
-a.getNew()
-print(a.deckid)
-print(a.getCard(10))
->>>>>>> 709e46517e5c5270314a3e9aea619bcfe3c929e3
