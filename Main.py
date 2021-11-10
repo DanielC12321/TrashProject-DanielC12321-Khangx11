b4fe39e7c7b0c23c8850f3692892d6038f5f252f
Khangnx03 <73293627+Khangnx03@users.noreply.github.com>
2021-11-10 07:43:00 -0600

    sl;ajdk

--- Main.py
+++ Main.py
@@ -1,24 +1,31 @@
 import pygame
+import io
 pygame.init()
 
-WIDTH, HEIGHT = 900, 500
+WIDTH, HEIGHT = 1200, 700
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Trash")
 
-WHITE = (255, 255, 255)
+GREEN = (0, 128, 0)
+
+FPS = 20
+
+
 
 def draw_window():
-    WIN.fill(WHITE)
+    WIN.fill(GREEN)
     pygame.display.update()
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type  == pygame.QUIT:
                 run = False
 
-       draw_window()
+        draw_window()
 
     pygame.quit()
 
--- deckmethods.py
+++ deckmethods.py
@@ -3,5 +3,5 @@ import json
 response = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
 deckid = (response.json())['deck_id']
 def getCard(numcards):
-    print(requests.get('https://deckofcardsapi.com/api/deck/{id}/draw/?count={number}'.format(id = String(deckid), number = int(numcards)).dumps())
-    pass
+    print(requests.get('https://deckofcardsapi.com/api/deck/{id}/draw/?count={number}'.format(id = str(deckid), number = int(numcards)).dumps()))
+getCard(5)
