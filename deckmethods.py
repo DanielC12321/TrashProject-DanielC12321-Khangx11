1f320c7f96efbb9c631772ae4b64cdee7765852c
DanielC12321 <73292458+DanielC12321@users.noreply.github.com>
2021-11-11 07:37:54 -0600

    Update deckmethods.py

--- deckmethods.py
+++ deckmethods.py
@@ -1,9 +1,5 @@
 import requests
 import json
-<<<<<<< HEAD
-
-=======
->>>>>>> 330ca68b8dc2595a6098a3e43ba5f4237e2e27c9
 class deckmethod:
     response = requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     discard = []
@@ -24,11 +20,3 @@ class deckmethod:
         self.deckid = (requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1").json())['deck_id']
         
 
-<<<<<<< HEAD
-a = deckmethod()
-print(a.deckid)
-a.getNew()
-print(a.deckid)
-print(a.getCard(10))
-=======
->>>>>>> 330ca68b8dc2595a6098a3e43ba5f4237e2e27c9
