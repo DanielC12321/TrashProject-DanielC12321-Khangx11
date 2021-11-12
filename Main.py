03c4097ec282ff93bd2a49846cb97bc8f69c383b
Khangnx03 <73293627+Khangnx03@users.noreply.github.com>
2021-11-12 07:36:28 -0600

    dafsa

--- Main.py
+++ Main.py
@@ -1,19 +1,33 @@
-from tkinter import *
-from tkinter import ttk
-import tkinter
-
+import tkinter as tk
+from tkinter.constants import NW
+import requests
+from io import BytesIO
+from PIL import Image, ImageTk
 from deckmethods import deckmethod
-root = tkinter.Tk()
+root = tk.Tk()
 
 
 
 
 a = deckmethod()
 
-canvas = tkinter.Canvas(root, bg ='green', height = 750, width = 1000)
+canvas = tk.Canvas(root, bg ='green', height = 750, width = 1000)
 
-
-
-
+card = a.getcard(1)
+url = card[0][1]
+response = requests.get(url)
+img_data = response.content
+img = ImageTk.PhotoImage(Image.open(BytesIO(img_data)).resize((113, 157)))
 canvas.pack()
+canvas.create_image(15, 15, anchor = NW, image = img)
+def round():
+    pass
+gameover = False
+while gameover == False:
+    round
+    
+
+
+
+
 root.mainloop()
